c724e5585ef459a63dcff2910c47dc9e379fd376
Gustavo <gustavoguimaraes@gmail.com>
2013-12-09 13:08:01 -0500

    Second Commit

--- Gustavo_first_Sinatra_app/app.rb
+++ Gustavo_first_Sinatra_app/app.rb
@@ -3,6 +3,11 @@ Bundler.require
 
 class TestApp < Sinatra::Application
 
+	get '/numbers' do
+    @random_numbers = (1..20).to_a.sample
+    erb :numbers
+  end
+
 	get '/artist' do
 		@artist_list = ["Nicole Kidman", "Tom Cruise", "Kurt Cobain", "Lady Gaga", "Britney Spears", "Beyonce", "Miley Cyrus"].sample
 		erb :index
