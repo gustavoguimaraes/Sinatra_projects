6dee3833442a6beb221d12aa64419ce147bec4b4
Gustavo <gustavoguimaraes@gmail.com>
2013-12-11 15:14:27 -0500

    Add RPS

--- Rock_paper_scissors/game.rb
+++ Rock_paper_scissors/game.rb
@@ -5,12 +5,39 @@ Bundler.require
 module Game 
   class RPS_App < Sinatra::Application
 
-    params = {:rock => "That's a rock", :paper => "That's paper", :scissors => "Scisscors!"}
+    # get '/' do
+    # "Hello World"
+    # end
 
-    get '/throw/rock' do
-      @result = params
-      erb :index 
+    get '/' do
+     erb :index
     end
+
+
+ 	get '/throw/move/:name' do
+		@human = params[:name]
+		@computer = ["rock", "paper", "scissors"].sample
+		while true
+			if @human == "rock" && @computer == "scissors"
+				@result = "Rock Beats Scissors. You Win!"
+				break
+			elsif @human == "paper" && @computer == "rock"
+				@result = "Paper Beats Rock. You Win!"
+				break
+			elsif @human == "scissors" && @computer == "paper"
+				@result = "Scissors Beats Paper. You Win!"
+				break
+			elsif @human == @computer
+			 	@result == "Tie. Play again"
+			else 
+				@result = "Computer Wins. You lose!"
+				break
+			end	
+		
+		end
+
+     erb :move
+  	end
 
   end
 end
