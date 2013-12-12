deaaea63242e9b4f15446a260f80e982b05a7eec
Gustavo <gustavoguimaraes@gmail.com>
2013-12-11 21:55:18 -0500

    Add Citi bike project

---
+++ citibike-sinatra/citibike-sinatra/app.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'bundler'
+require "sinatra/reloader"
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Citibike
+	class App < Sinatra::Application
+    configure :development do
+      register Sinatra::Reloader
+    end
+
+    before do
+      json = File.open("data/citibikenyc.json").read
+      @data = MultiJson.load(json)
+    end
+
+    get '/' do
+      erb :home
+    end
+
+    get'/form' do
+      erb :form
+    end
+
+    post '/map' do
+      @start_station = params["start"]
+      @end_station = params["end"]
+      erb :map
+    end
+
+
+  end
+end
